50a2f692e580eea07a172690d3ab7c5df5e0d778
Ondrej Cifka <ondra@cifka.com>
2015-06-07 21:53:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 setup(
-        name="wikibrev",
+        name='wikibrev',
+        version='0.0',
+        url='https://github.com/cifkao/wikibrev',
+        description='A tool for extracting abbreviations from Wikipedia dumps',
         packages = ['wikibrev'],
         ext_modules = cythonize(Extension(
-            "wikibrev._unzim",
-            sources=["wikibrev/_unzim.pyx"],
-            libraries=["zim", "stdc++"],
-            language="c++",
+            'wikibrev._unzim',
+            sources=['wikibrev/_unzim.pyx'],
+            libraries=['zim', 'stdc++'],
+            language='c++',
         )),
-        scripts=['bin/wikibrev']
+        scripts=['bin/wikibrev'],
+        install_requires=['magic']
 )
