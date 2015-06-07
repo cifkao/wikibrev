e13a5716f490d8112e05b009b8f9e37911b190c0
Ondrej Cifka <ondra@cifka.com>
2015-06-07 22:27:23 +0200

    Remove Cython requirement

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
 from setuptools import setup, Extension
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+    HAVE_CYTHON = True
+except ImportError as e:
+    HAVE_CYTHON = False
+
+extensions = [Extension(
+    'wikibrev._unzim',
+    sources=[
+        'wikibrev/_unzim.pyx' if HAVE_CYTHON else 'wikibrev/_unzim.cpp'
+    ],
+    libraries=['zim', 'stdc++'],
+    language='c++'
+)]
+
+if HAVE_CYTHON:
+    extensions = cythonize(extensions)
 
 setup(
         name='wikibrev',
@@ -9,12 +25,7 @@ setup(
         url='https://github.com/cifkao/wikibrev',
         description='A tool for extracting abbreviations from Wikipedia dumps',
         packages = ['wikibrev'],
-        ext_modules = cythonize(Extension(
-            'wikibrev._unzim',
-            sources=['wikibrev/_unzim.pyx'],
-            libraries=['zim', 'stdc++'],
-            language='c++',
-        )),
+        ext_modules = extensions,
         scripts=['bin/wikibrev'],
         install_requires=['magic']
 )
